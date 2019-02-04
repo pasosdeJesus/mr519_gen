3740d0c1983c09f6e64cf9ba611f57f6fdb47afc
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-02-03 20:24:39 -0500

    Actualiza

--- test/dummy/config/initializers/sip.rb
+++ test/dummy/config/initializers/sip.rb
@@ -2,7 +2,7 @@
 
 Sip.setup do |config|
       config.ruta_anexos = "#{Rails.root}/archivos/anexos/"
-      config.ruta_volcados = "#{Rails.root}/archivos/volcados/"
+      config.ruta_volcados = "#{Rails.root}/archivos/bd/"
       # En heroku los anexos son super-temporales
       if !ENV["HEROKU_POSTGRESQL_GREEN_URL"].nil?
         config.ruta_anexos = "#{Rails.root}/tmp/"
