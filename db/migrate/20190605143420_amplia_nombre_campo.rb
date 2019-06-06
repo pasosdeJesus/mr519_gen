8e24cf5003643dae1cf872aaf8c14752135f4956
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-06-06 09:22:08 -0500

    En campos de formulario amplia nombre a 512

--- db/migrate/20190605143420_amplia_nombre_campo.rb
+++ db/migrate/20190605143420_amplia_nombre_campo.rb
@@ -1,7 +1,10 @@
 # encoding: UTF-8
 
 class AmpliaNombreCampo < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :mr519_gen_campo, :nombre, :string, limit: 512
+  end
+  def down
+    change_column :mr519_gen_campo, :nombre, :string, limit: 128
   end
 end
