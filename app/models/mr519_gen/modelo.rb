ee57bc921ba493b7f105669e97dbe129f4a07c79
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-07-02 10:53:17 -0500

    Modulo modelo que amplia el de Sip pero presentando datos respondidos para un formulario

---
+++ app/models/mr519_gen/modelo.rb
@@ -0,0 +1,60 @@
+# encoding: UTF-8
+
+# Extiende Sip::Modelo para facilitar presentar
+# información de formularios
+module Mr519Gen
+  module Modelo 
+    extend ActiveSupport::Concern
+
+    included do
+      include Sip::Modelo
+
+      def presenta_gen_mr519(atr)
+        if atr.to_s.include?(".")
+          pa = atr.split('.')
+          f = Mr519Gen::Formulario.
+            where(nombreinterno: pa[0])
+          if f.count == 1
+            fr = f.take
+            if self.respuestafor && 
+              self.respuestafor.where(
+                formulario_id:  fr.id).count == 1
+              rf = self.respuestafor.where(
+                formulario_id: fr.id).take
+              ca = fr.campo.where(nombreinterno: pa[1])
+              if ca.count == 1
+                vc = rf.valorcampo.where(
+                  campo_id: ca.take.id)
+                if vc.count == 1
+                  if ca.take.tipo == Mr519Gen::ApplicationHelper::SELECCIONMULTIPLE
+                    return vc.take.valorjson.to_s
+                  else
+                    return vc.take.valor.to_s
+                  end
+                else
+                  return "Hay más de un valor para campo '#{pa[1]}' de formulario '#{pa[0]}'"
+                end
+              else
+                return "No hay campo '#{pa[1]}' en formulario '#{pa[0]}'"
+              end
+            else
+              return "No hay respuesta para formulario '#{pa[0]}"
+            end
+          elsif f.count == 0
+            return "No hay formulario '#{pa[0]}'"
+          else
+            return "Varios formularios '#{pa[0]}'"
+          end
+          
+        else
+          presenta_gen_sip(atr)
+        end
+      end
+
+      def presenta_gen(atr)
+        presenta_gen_mr519(atr)
+      end
+
+    end
+  end
+end
