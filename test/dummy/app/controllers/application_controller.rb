1f648dee04d5dbb93a95389706292b99dccff187
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-12-30 17:35:32 -0500

    Traducción

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ require 'sip/application_controller'
 class ApplicationController < Sip::ApplicationController
   # Previente ataques CSRF elevando una excepción
   # En el caso de APIs, en cambio puedes querer usar :null_session
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 end
