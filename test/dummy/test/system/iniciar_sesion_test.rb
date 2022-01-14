6b8dda230022dfd6ee80ec5c5f4fb2c7f73897e5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-14 16:28:04 -0500

    Usa auxiliares para iniciar sesion en pruebas del sistema

--- test/dummy/test/system/iniciar_sesion_test.rb
+++ test/dummy/test/system/iniciar_sesion_test.rb
@@ -2,19 +2,8 @@ require "application_system_test_case"
 
 class IniciarSesionTest < ApplicationSystemTestCase
 
-  def iniciar_sesion(usuario, clave)
-    visit root_url
-    assert_content 'Pasos de Jesús'
-    click_link 'Iniciar Sesión'
-    assert_content 'Usuario'
-    fill_in('Usuario', with: usuario)
-    fill_in('Clave', with: clave)
-    find_button('Iniciar Sesión').click
-    assert_content 'Sesión iniciada.'
-  end
-
   test "iniciar sesión" do
-    iniciar_sesion('mr519', 'mr519')
+    Sip::CapybaraHelper.iniciar_sesion(self, root_path, 'mr519', 'mr519')
   end
 
 end
