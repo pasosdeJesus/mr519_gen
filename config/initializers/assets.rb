f97ba2608792fd22b5f3af65a0a552055e07ea29
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-03-24 23:56:36 -0500

    Actualiza

--- config/initializers/assets.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.assets.paths << Rails.root.join('node_modules/gristack')
-
-Rails.application.config.assets.precompile << 'gridstack.js'
-Rails.application.config.assets.precompile << 'gridstack.jQueryUI.js'
