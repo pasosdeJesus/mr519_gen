ebe6633106d5d201093aedec96336687b6734305
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-05-16 11:33:33 -0500

    Actualiza

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -990,6 +990,13 @@ CREATE TABLE public.sip_grupoper (
 
 
 --
+-- Name: TABLE sip_grupoper; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE public.sip_grupoper IS 'Creado por sip en sipdes_des';
+
+
+--
 -- Name: sip_grupoper_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
