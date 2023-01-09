d95fe48daef048fdd05bb69ae56056d201f1a1d8
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-08 19:49:22 -0500

    Actualiza

--- bin/regresion.sh
+++ bin/regresion.sh
@@ -4,11 +4,15 @@
 # directorio coverage y publicandolo HTML en public/mimotor/cobertura
 
 rutaap="./"
-if (test -f test/dummy/config/application.rb)  then {
+if (test -f "test/dummy/config/application.rb")  then {
   rutaap="test/dummy/"
-  . test/dummy/.env
 } else {
-  . ./.env
+  rutaap="./"
+} fi;
+if (test -f $rutaap/.env) then {
+  (cd $rutaap; . ./.env)
+} else {
+  echo "No existe $rutaap/.env";
 } fi;
 
 echo "== Prepara"
@@ -18,61 +22,59 @@ if (test "$?" != "0") then {
   exit 1;
 } fi;
 
+echo "== Prepara entorno"
+
+vm=`ruby -v | sed  -e "s/^ruby \([0-9]\.[0-9]\).[0-9]* .*/\1/g"`
+if (test "$vm" = "") then {
+  echo "No se detectó versión de ruby"
+  exit 1;
+} fi;
+
+RAILS="bin/rails"
+if (test "$vm" = "3.2") then {
+  RAILS="ruby --yjit bin/rails"
+} fi;
+
+if (test -f "test/dummy/config/application.rb") then {
+  (cd test/dummy; ${RAILS} db:environment:set RAILS_ENV=test; RAILS_ENV=test ${RAILS} db:drop db:create db:setup db:seed msip:indices)
+} else {
+  ${RAILS} db:environment:set RAILS_ENV=test; 
+  RAILS_ENV=test ${RAILS} db:drop db:create db:setup db:seed msip:indices
+} fi;
+
 echo "== Pruebas de regresión unitarias"
 mkdir -p cobertura-unitarias/
 rm -rf cobertura-unitarias/{*,.*}
-CONFIG_HOSTS=www.example.com bin/rails test
+CONFIG_HOSTS=www.example.com ${RAILS} test
 
 echo "== Pruebas de regresión al sistema"
 if (test -f "test/dummy/config/application.rb") then {
   mkdir -p test/dummy/cobertura-sistema/
   rm -rf test/dummy/cobertura-sistema/{*,.*}
-  (cd test/dummy; CONFIG_HOSTS=127.0.0.1 bin/rails msip:stimulus_motores test:system)
+  if (test "$CI" = "") then { # Por ahora no en gitlab-ci
+    (cd test/dummy; CONFIG_HOSTS=127.0.0.1 ${RAILS} msip:stimulus_motores test:system)
+  } fi;
+  if (test -f test/dummy/bin/pruebasjs) then {
+    (cd test/dummy; CONFIG_HOSTS=127.0.0.1 ${RAILS} msip:stimulus_motores; bin/pruebasjs)
+  } fi;
 } else {
   mkdir -p cobertura-sistema/
   rm -rf cobertura-sistema/{*,.*}
-  CONFIG_HOSTS=127.0.0.1 bin/rails msip:stimulus_motores test:system
+  if (test "$CI" = "") then { # Por ahora no en gitlab-ci
+    CONFIG_HOSTS=127.0.0.1 ${RAILS} msip:stimulus_motores test:system
+  } fi;
+  if (test -f bin/pruebasjs) then {
+    (CONFIG_HOSTS=127.0.0.1 ${RAILS} msip:stimulus_motores; bin/pruebasjs)
+  } fi;
 } fi;
 
 echo "== Unificando resultados de pruebas en directorio clásico coverage"
 mkdir -p coverage/
 rm -rf coverage/{*,.*}
-bin/rails app:msip:reporteregresion
+${RAILS} app:msip:reporteregresion
 
 echo "== Copiando resultados para hacerlos visibles en el web en ruta cobertura"
 # Copiar resultados para hacerlos visibles en web
 mkdir -p test/dummy/public/msip/cobertura/
 cp -rf coverage/* test/dummy/public/msip/cobertura/
 cp -rf coverage/assets/* test/dummy/public/msip/assets/
-#!/bin/sh
-# Inicializa base de prueba, corre 2 pruebas de regresión (test y test:system) 
-# y finalmente genera reporte de cobertura mezclando resultados en 
-# directorio coverage y publicandolo HTML en public/mimotor/cobertura
-
-
-echo "== Pruebas de regresión unitarias"
-mkdir -p cobertura-unitarias/
-rm -rf cobertura-unitarias/{*,.*}
-CONFIG_HOSTS=www.example.com bin/rails test
-
-echo "== Pruebas de regresión al sistema"
-if (test -f "test/dummy/config/application.rb") then {
-  mkdir -p test/dummy/cobertura-sistema/
-  rm -rf test/dummy/cobertura-sistema/{*,.*}
-  (cd test/dummy; CONFIG_HOSTS=127.0.0.1 bin/rails msip:stimulus_motores test:system)
-} else {
-  mkdir -p cobertura-sistema/
-  rm -rf cobertura-sistema/{*,.*}
-  CONFIG_HOSTS=127.0.0.1 bin/rails msip:stimulus_motores test:system
-} fi;
-
-echo "== Unificando resultados de pruebas en directorio clásico coverage"
-mkdir -p coverage/
-rm -rf coverage/{*,.*}
-bin/rails app:msip:reporteregresion
-
-echo "== Copiando resultados para hacerlos visibles en el web en ruta cobertura"
-# Copiar resultados para hacerlos visibles en web
-mkdir -p test/dummy/public/${RUTA_RELATIVA}/cobertura/
-cp -rf coverage/* test/dummy/public/${RUTA_RELATIVA}/cobertura/
-cp -rf coverage/assets/* test/dummy/public/${RUTA_RELATIVA}/assets/
