0714aa1298dcbd912260450dd10e6f106e5fe5a8
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-07-11 04:09:04 -0500

    Variables MSIP

--- test/dummy/config/initializers/msip.rb
+++ test/dummy/config/initializers/msip.rb
@@ -4,11 +4,11 @@ require "mr519_gen/version"
 
 Msip.setup do |config|
   config.ruta_anexos = ENV.fetch(
-    "SIP_RUTA_ANEXOS",
+    "MSIP_RUTA_ANEXOS",
     "#{Rails.root}/archivos/anexos",
   )
   config.ruta_volcados = ENV.fetch(
-    "SIP_RUTA_VOLCADOS",
+    "MSIP_RUTA_VOLCADOS",
     "#{Rails.root}/archivos/bd",
   )
   # En heroku los anexos son super-temporales
