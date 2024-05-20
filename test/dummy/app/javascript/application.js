48d10c7fa4c3db696c9dedc902382980397555d2
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-05-20 16:27:18 -0500

    chosen cambiado por tom-select. Closes #29

--- test/dummy/app/javascript/application.js
+++ test/dummy/app/javascript/application.js
@@ -13,15 +13,27 @@ import 'popper.js'              // Dialogos emergentes usados por bootstrap
 import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'gridstack'
 
+import TomSelect from 'tom-select';
+window.TomSelect = TomSelect;
+window.configuracionTomSelect = {
+  create: false,
+  diacritics: true, //no sensitivo a acentos
+  sortField: {
+    field: "text",
+    direction: "asc"
+  }
+}
+
 import Msip__Motor from "./controllers/msip/motor"
 window.Msip__Motor = Msip__Motor
+Msip__Motor.iniciar()
 import Mr519Gen__Motor from "./controllers/mr519_gen/motor"
 window.Mr519Gen__Motor = Mr519Gen__Motor
+
 
 import {AutocompletaAjaxExpreg} from '@pasosdejesus/autocompleta_ajax'
 window.AutocompletaAjaxExpreg = AutocompletaAjaxExpreg
 
-import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
 
@@ -46,6 +58,8 @@ promesaRecursosSprocketsYDocumento.then((mensaje) => {
   console.log(mensaje)
   var root;
   root = window;
+
+  Msip__Motor.ejecutarAlCargarDocumentoYRecursos()  // Este se ejecuta cada vez que se carga una página que no está en cache y tipicamente después de que se ha cargado la página completa y los recursos
   msip_prepara_eventos_comunes(root);
   mr519_gen_prepara_eventos_comunes(root);
 })
