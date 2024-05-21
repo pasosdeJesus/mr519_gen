979d0555c4b17687c9d553783025c7050362a063
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-05-21 13:55:42 -0500

    Avanza cambio de cocoon a turbo #30. Cada fila del formulario

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 Mr519Gen::Engine.routes.draw do
-  resources :campos, only: [:new, :destroy]
+
+  resources :campos, only: [], param: :index do
+    member do
+      delete '(:id)', to: "campos#destroy", as: "eliminar"
+      post '/' => "campos#create", as: "crear"
+    end
+  end
+
   resources :encuestaspersona, path_names: { new: "nueva", edit: "edita" }
   resources :encuestasusuario, path_names: { new: "nueva", edit: "edita" }
   resources :formularios, path_names: { new: "nuevo", edit: "edita" }
