6f8c2646b5fde0cb147737a4c72e96b0b9926012
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-06-20 06:21:22 -0500

    Quitar datepicker. Closes #31

--- lib/mr519_gen/concerns/models/encuestausuario.rb
+++ lib/mr519_gen/concerns/models/encuestausuario.rb
@@ -57,32 +57,32 @@ module Mr519Gen
             end
           end
 
-          attr_accessor :fechaini_localizada
+          attr_accessor :fechaini
 
-          def fechaini_localizada
+          def fechaini
             if !respuestafor.nil? &&
-                !respuestafor.fechaini_localizada.nil?
-              respuestafor.fechaini_localizada
+                !respuestafor.fechaini.nil?
+              respuestafor.fechaini
             end
           end
 
-          def fechaini_localizada=(val)
+          def fechaini=(val)
             self.respuestafor = Mr519Gen::Respuestafor.new if respuestafor.nil?
-            respuestafor.fechaini_localizada = val
+            respuestafor.fechaini = val
           end
 
-          attr_accessor :fechacambio_localizada
+          attr_accessor :fechacambio
 
-          def fechacambio_localizada
+          def fechacambio
             if !respuestafor.nil? &&
-                !respuestafor.fechacambio_localizada.nil?
-              respuestafor.fechacambio_localizada
+                !respuestafor.fechacambio.nil?
+              respuestafor.fechacambio
             end
           end
 
-          def fechacambio_localizada=(val)
+          def fechacambio=(val)
             self.respuestafor = Mr519Gen::Respuestafor.new if respuestafor.nil?
-            respuestafor.fechacambio_localizada = val
+            respuestafor.fechacambio= val
           end
 
           attr_accessor :formulario_id
