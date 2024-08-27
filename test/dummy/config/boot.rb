c7c8ddba937803e4f6a18b31863eb3f39ebbad5f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-08-27 09:47:11 -0500

    Actualizacion parcial a rails 7.2. Quedo mejor la de la rama main

--- test/dummy/config/boot.rb
+++ test/dummy/config/boot.rb
@@ -2,4 +2,6 @@
 
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bundler/setup" # Configurar gemas listadas en Gemfile
+require "bootsnap/setup" # Acelerar tiempo de arranque dejando en colchón operaciones costosas
+$LOAD_PATH.unshift(File.expand_path("../../../../lib", __FILE__))
