84639b40d6a9b2066f8eb647756f95d1ab8c32d9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-09 19:09:21 -0400

    Documenta clases

--- app/models/mr519_gen/opcioncs.rb
+++ app/models/mr519_gen/opcioncs.rb
@@ -3,6 +3,7 @@
 require "mr519_gen/concerns/models/opcioncs"
 
 module Mr519Gen
+  # Opción en un campo de selección
   class Opcioncs < ActiveRecord::Base
     include Mr519Gen::Concerns::Models::Opcioncs
   end
--- app/models/mr519_gen/valorcampo.rb
+++ app/models/mr519_gen/valorcampo.rb
@@ -3,6 +3,7 @@
 require "mr519_gen/concerns/models/valorcampo"
 
 module Mr519Gen
+  # Valor que un usuario/persona diligencia en un campo de un formulario
   class Valorcampo < ActiveRecord::Base
     include Mr519Gen::Concerns::Models::Valorcampo
   end
