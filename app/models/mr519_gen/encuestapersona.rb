097ed3f3b005d0c5d968c5c38b6f7b485fcaaf5f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-09 19:09:21 -0400

    Documenta clases

--- app/models/mr519_gen/encuestapersona.rb
+++ app/models/mr519_gen/encuestapersona.rb
@@ -3,6 +3,8 @@
 require "mr519_gen/concerns/models/encuestapersona"
 
 module Mr519Gen
+  # Encuesta a una persona externa (i.e que no tiene cuenta en el sistema de
+  # información)
   class Encuestapersona < ActiveRecord::Base
     include Mr519Gen::Concerns::Models::Encuestapersona
   end
--- app/models/mr519_gen/planencuesta.rb
+++ app/models/mr519_gen/planencuesta.rb
@@ -3,6 +3,7 @@
 require "mr519_gen/concerns/models/planencuesta"
 
 module Mr519Gen
+  # Plan para una encuesta
   class Planencuesta < ActiveRecord::Base
     include Mr519Gen::Concerns::Models::Planencuesta
   end
