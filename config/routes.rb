e4e23ac779234c41f7a200f364eb6709a37a3b2b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-02-18 20:18:10 -0500

    rutas

--- config/routes.rb
+++ config/routes.rb
@@ -4,35 +4,35 @@ Mr519Gen::Engine.routes.draw do
   resources :campos, only: [], param: :index do
     member do
       delete "(:id)", to: "campos#destroy", as: "eliminar"
-      post "/" => "campos#create", as: "crear"
+      post "/", to: "campos#create", as: "crear"
     end
   end
 
-  resources :opcionescs, only: [], param: :index do
-    member do
-      delete "(:id)", to: "opcionescs#destroy", as: "eliminar"
-      post "/" => "opcionescs#create", as: "crear"
-    end
-  end
+  get "/encuestaexterna/:adurl", to: "encuestaspersona#externa",
+    as: :encuestaexterna
 
   resources :encuestaspersona, path_names: { new: "nueva", edit: "edita" }
+
   resources :encuestasusuario, path_names: { new: "nueva", edit: "edita" }
-  resources :formularios, path_names: { new: "nuevo", edit: "edita" }
-  get "/formularios/copia/:formulario_id" =>
-  "formularios#copia",
-    as: :copia_formulario
-
-  resources :planesencuesta,
-    path_names: { new: "nuevo", edit: "edita" }
-
   get "/encuestasusuario/resultados/:encuestausuario_id" =>
     "encuestasusuario#resultados",
     as: :resultadosencuestausuario
-
   get "/encuestasusuario/creartodousuario/:encuestausuario_id" =>
   "encuestasusuario#creartodousuario",
     as: :creartodousuario
 
-  get "/encuestaexterna/:adurl" => "encuestaspersona#externa",
-    as: :encuestaexterna
+  resources :formularios, path_names: { new: "nuevo", edit: "edita" }
+  get "/formularios/copia/:formulario_id", to: "formularios#copia",
+    as: :copia_formulario
+
+  resources :opcionescs, only: [], param: :index do
+    member do
+      delete "(:id)", to: "opcionescs#destroy", as: "eliminar"
+      post "/", to: "opcionescs#create", as: "crear"
+    end
+  end
+
+  resources :planesencuesta,
+    path_names: { new: "nuevo", edit: "edita" }
+
 end
