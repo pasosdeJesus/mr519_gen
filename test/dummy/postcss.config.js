24ea30d5bbe4e587a802d2c49249474ba6abd535
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-03-25 13:20:22 -0500

    Revert "Actualiza a rails 8 y a propshaft+cssbundling-rails+postcss"
    This reverts commit b5194fc65e22800951efee461e4580d4f8ee3edb.

--- test/dummy/postcss.config.js
+++ test/dummy/postcss.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-nesting'),
-    require('autoprefixer'),
-  ],
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ]
 }
